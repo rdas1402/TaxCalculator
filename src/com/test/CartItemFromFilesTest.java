9cfebb90808fa7ada000651dc29b607e68e85bda
infinity <r14.das@gmail.com>
2020-08-21 22:43:25 +0530

    New_code

---
+++ src/com/test/CartItemFromFilesTest.java
@@ -0,0 +1,51 @@
+/*
+* <h1>Calculate Tax on goods</h1> 
+* Basic sales tax is applicable at a rate of 10% on all goods, except books, food, and medical products that are exempt. Import duty is
+* an additional sales tax applicable on all imported goods at a rate of 5%, with no exemptions.
+* When I purchase items, I receive a receipt which lists the name of all the items and their price (including tax), finishing with the total
+* cost of the items, and the total amounts of sales taxes paid. The rounding rules for sales tax are that for a tax rate of n%, a shelf price
+* of p contains (np/100 rounded up to the nearest 0.05) amount of sales tax.
+* 
+* This class servers as a Junit Test case class
+* 
+* @author  Rupam Das 
+* @since   2017-08-21 
+*/
+
+package com.test;
+
+import org.junit.Test;
+
+import com.service.TaxService;
+
+
+
+public class CartItemFromFilesTest {
+
+	@Test 
+	public void testFileEntry1() {
+		TaxService.getFromFile("source/in1.txt");
+	}
+		
+		
+	@Test
+	public void testFileEntry2() {
+		TaxService.getFromFile("source/in2.txt");
+	}
+	
+	@Test
+	public void testFileEntry3() {
+		TaxService.getFromFile("source/in3.txt");
+	}
+	
+	@Test
+	public void testFileEntry4() {
+		TaxService.getFromFile("source/in4.txt");
+	}
+	
+	@Test
+	public void testFileEntry5() {
+		TaxService.getFromFile("source/in5.txt");
+	}
+
+}
